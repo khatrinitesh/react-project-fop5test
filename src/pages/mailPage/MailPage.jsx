0cd3fde52e07640d9f188b0dc7decdfd8fff2964
nitesh <nitesh.khatri88@gmail.com>
2026-01-30 11:07:04 +0530

    updated 1

--- src/pages/mailPage/MailPage.jsx
+++ src/pages/mailPage/MailPage.jsx
@@ -19,25 +19,27 @@ const MailTabbed = () => {
   };
 
   return (
-    <div className="bg-[#e6edf7] border border-[#0b2c6b]">
-      {/* Tabs */}
-      <ul className="flex border-b border-[#0b2c6b] bg-[#f2f5ff]">
-        {tabs.map((tab) => (
-          <li key={tab.id}>
-            <button
-              onClick={() => setActiveTab(tab.id)}
-              className={`cursor-pointer text-black font-semibold px-4 py-1 text-xs border-r border-[#0b2c6b]
+    <div className="bg-[#e6edf7]  h-full p-5 ">
+      <div className="shadow-md rounded-md overflow-hidden border border-[#0b2c6b]">
+        {/* Tabs */}
+        <ul className="flex border-b  border-[#0b2c6b] bg-[#f2f5ff]">
+          {tabs.map((tab) => (
+            <li key={tab.id}>
+              <button
+                onClick={() => setActiveTab(tab.id)}
+                className={`cursor-pointer text-black font-semibold px-4 py-2 text-xs border-r border-[#0b2c6b]
                 ${activeTab === tab.id ? "bg-[#1b7398] text-white" : "bg-[#f2f5ff] text-black"}
               `}
-            >
-              {tab.label}
-            </button>
-          </li>
-        ))}
-      </ul>
+              >
+                {tab.label}
+              </button>
+            </li>
+          ))}
+        </ul>
 
-      {/* Content */}
-      <div className="p-4 bg-white">{renderTab()}</div>
+        {/* Content */}
+        <div className="p-4 bg-white">{renderTab()}</div>
+      </div>
     </div>
   );
 };
